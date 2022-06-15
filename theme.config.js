9d87b3d28d1887555f461373637d399452fda80e
Bunlong <bunlong.van@gmail.com>
2022-06-16 00:07:21 +0700

    Add Cardinal Direction section

--- theme.config.js
+++ theme.config.js
@@ -1,7 +1,7 @@
 export default {
   github: 'https://github.com/Bunlong/next-faker',
   docsRepositoryBase: 'https://github.com/Bunlong/next-faker',
-  titleSuffix: ' – Nextra',
+  titleSuffix: ' – next-faker',
   logo: (
     <>
       <span className="mr-2 font-extrabold hidden md:inline">next-faker</span>
@@ -16,15 +16,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="next-faker: React hook for generating fake data for testing and development." />
+      <meta name="og:description" content="next-faker: React hook for generating fake data for testing and development." />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="twitter:image" content="/next-faker.png" />
+      <meta name="twitter:site:domain" content="next-faker.github.io/" />
+      <meta name="twitter:url" content="https://next-faker.github.io/" />
+      <meta name="og:title" content="next-faker: React hook for generating fake data for testing and development." />
+      <meta name="og:image" content="/next-faker.png" />
+      <meta name="apple-mobile-web-app-title" content="next-faker" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
